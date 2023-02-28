66f80bb596ceb4b21bbe9071dd69f78e93e60dbd
Isaque <isaquefrasciawerner@gmail.com>
2023-02-28 00:19:25 -0300

    Primeiro commit do repositório

---
+++ helloworld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    //Entrada e Saida e os tipos primitivos - Estruturas sequenciais
+    int a;
+    int b;
+    printf("Digite o primeiro numero:\n");
+    
+    scanf("%d", &a);
+
+    printf("Digite o segundo numero:\n");
+
+    scanf("%d", &b);
+
+    int resultado = a + b;
+
+    printf("%d", resultado);
+
+
+
+
+}
